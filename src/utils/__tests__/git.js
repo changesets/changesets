c2afe84ba66775a5506700aef66c1d0847e07c7c
Ben Conolly <ben@thinkmill.com.au>
2019-05-01 12:03:29 +1000

    start testing git maybe

---
+++ src/utils/__tests__/git.js
@@ -0,0 +1,29 @@
+import { copyFixtureIntoTempDir } from "jest-fixtures";
+
+describe("git", () => {
+  describe("getCommitThatAddsFile", () => {
+    it("should have tests", () => false);
+  });
+  describe("getChangedFilesSince", () => {
+    it("should have tests", () => false);
+  });
+  describe("add", () => {
+    it("should have tests", () => false);
+  });
+  describe("commit", () => {
+    it("should have tests", () => false);
+  });
+  describe("tag", () => {
+    it("should have tests", () => false);
+  });
+  describe("getChangedPackagesSinceMaster", () => {
+    it("should have tests", () => false);
+  });
+  describe("getChangedChangesetFilesSinceMaste", () => {
+    it("should have tests", () => false);
+  });
+
+  it("should have tests", async () => {
+    const cwd = await copyFixtureIntoTempDir(__dirname, "with-git");
+  });
+});
