f20570cb8f27d8e198c8ac898730e6294d54139b
Peter Yu <pyu@atlassian.com>
2019-05-06 11:56:51 +1000

    Only display "All packages" options if there are more than 10 packages

--- packages/cli/src/commands/add/createChangeset.js
+++ packages/cli/src/commands/add/createChangeset.js
@@ -52,9 +52,11 @@ async function getPackagesToRelease(changedPackages, allPackages) {
       .map(({ name }) => name)
       .filter(name => !changedPackages.includes(name));
 
+    const selectAllPackagesOptions = allPackages.length > 10 ?
+      ["All packages", "All changed packages"] : [];
+
     const defaultInquirerList = [
-      "All packages",
-      "All changed packages",
+      ...selectAllPackagesOptions,
       new inquirer.Separator("changed packages"),
       ...changedPackages,
       new inquirer.Separator("unchanged packages"),
