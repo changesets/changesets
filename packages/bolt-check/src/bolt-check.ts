553dbf3d9f70c826e3aa27eed44058f502812f0f
noviny <novinyarts@gmail.com>
2019-06-02 01:29:49 +1000

    well it works now

--- packages/bolt-check/src/bolt-check.ts
+++ packages/bolt-check/src/bolt-check.ts
@@ -5,6 +5,12 @@ import fs from "fs-extra";
 import chalk from "chalk";
 import detectIndent from "detect-indent";
 
+function versionRangeToRangeType(versionRange: string) {
+  if (versionRange.charAt(0) === "^") return "^";
+  if (versionRange.charAt(0) === "~") return "~";
+  return "";
+}
+
 const DEPENDENCY_TYPES = [
   "dependencies",
   "devDependencies",
@@ -54,10 +60,10 @@ const getExternalErrorMessage = ({
   rootVersion,
   pkgVersion
 }: ExternalMismatch) =>
-  chalk`{yellow ${pkgName}} relies on {yellow ${pkgVersion}} in {green ${dependency}}, but on {yellow ${rootVersion}} at the project root.`;
+  chalk`{yellow ${pkgName}} relies on {green ${dependency}} at {red ${pkgVersion}}, but your project relies on  {green ${dependency}} at {green ${rootVersion}}.`;
 
 // TODO: This function could sort, and order these errors to make nicer output. Not doing that for now.
-const printErrors = (errors: ErrorObj[]) => {
+let printErrors = (errors: ErrorObj[]) => {
   errors.forEach(error => {
     switch (error.type) {
       case "internalMismatch":
@@ -81,7 +87,7 @@ type workspaceConfig = {
 };
 
 const flatten = (workspace: workspaceConfig) => {
-  const flatDeps = new Map();
+  let flatDeps = new Map();
 
   for (let depType of DEPENDENCY_TYPES) {
     let deps = workspace[depType];
@@ -100,28 +106,28 @@ export default async function boltCheck(config: {
   silent?: boolean;
   fix?: boolean;
 }) {
-  const errors: ErrorObj[] = [];
+  let errors: ErrorObj[] = [];
 
-  const workspaces = await getWorkspaces(config);
+  let workspaces = await getWorkspaces(config);
 
   if (!workspaces) {
     console.error("could not resolve workspaces to check");
     throw new Error("could not resolve workspaces to check");
   }
 
-  const workspaceVersions = new Map();
-  const rootPkgJson: workspaceConfig = JSON.parse(
+  let workspaceVersions = new Map();
+  let rootPkgJson: workspaceConfig = JSON.parse(
     await fs.readFile(path.resolve(config.cwd, "package.json"), "utf-8")
   );
 
-  const rootDeps = flatten(rootPkgJson);
+  let rootDeps = flatten(rootPkgJson);
 
   for (let workspace of workspaces) {
     workspaceVersions.set(workspace.config.name, workspace.config.version);
   }
 
   for (let workspace of workspaces) {
-    const flatDeps = flatten(workspace.config);
+    let flatDeps = flatten(workspace.config);
     for (let [name, range] of flatDeps.entries()) {
       if (workspaceVersions.has(name)) {
         let currentVersion = workspaceVersions.get(name);
@@ -164,8 +170,8 @@ export default async function boltCheck(config: {
     printErrors(errors);
     process.exit(1);
   } else if (errors.length > 0 && config.fix) {
-    const missingDepErrors = [];
-    const otherErrors = [];
+    let missingDepErrors = [];
+    let otherErrors = [];
 
     for (let error of errors) {
       if (error.type === "missingDependency") {
@@ -179,16 +185,14 @@ export default async function boltCheck(config: {
       await fixMissingDependencies(missingDepErrors, config.cwd);
     }
 
-    // We want to sort other errors by workspace, so we can handle a
-    // workspace all at once
-
-    const workSpacesToUpdate: {
+    let workSpacesToUpdate: {
       [key: string]: { pkgDir: string; errors: PkgErrors[] };
     } = {};
 
     otherErrors.forEach(error => {
       if (!workSpacesToUpdate[error.pkgName]) {
-        const workspace = workspaces.find(w => w.name === error.pkgName);
+        // @ts-ignore
+        let workspace = workspaces.find(w => w.name === error.pkgName);
         if (!workspace) {
           throw new Error("congrats! you found an impossible error");
         }
@@ -202,47 +206,66 @@ export default async function boltCheck(config: {
     });
 
     await Object.values(workSpacesToUpdate).map(async ({ pkgDir, errors }) =>
-      fix(errors, pkgDir)
+      fixPkgErrors(errors, pkgDir)
     );
   } else {
     console.log("Looks like your dependencies are fine");
   }
 }
 
-const fix = async (errors: PkgErrors[], pkgDir: string) => {
-  const pkgPath = path.resolve(pkgDir, "package.json");
-  const pkgRaw = await fs.readFile(pkgPath, "utf-8");
-  const indent = detectIndent(pkgRaw).indent || "  ";
-  const pkg = JSON.parse(pkgRaw);
+const fixPkgErrors = async (errors: PkgErrors[], pkgDir: string) => {
+  let pkgPath = path.resolve(pkgDir, "package.json");
+  let pkgRaw = await fs.readFile(pkgPath, "utf-8");
+  let indent = detectIndent(pkgRaw).indent || "  ";
+  let pkg = JSON.parse(pkgRaw);
 
-  errors.forEach(() => {});
+  errors.forEach((error) => {
+    if (error.type === 'internalMismatch') {
+      let { dependency, version } = error;
+      DEPENDENCY_TYPES.forEach((depType) => {
+        if (pkg[depType] && pkg[depType][dependency]) {
+          let currentRange = pkg[depType][dependency]
+          let rangeType = versionRangeToRangeType(currentRange)
+          pkg[depType][dependency] = rangeType + version;
+        }
+      })
+    }
+    if (error.type === 'externalMismatch') {
+      DEPENDENCY_TYPES.forEach((depType) => {
+        let { dependency, rootVersion } = error;
+        if (pkg[depType] && pkg[depType][dependency]) {
+          pkg[depType][dependency] = rootVersion;
+        }
+      })
+    }
+  });
 
   await fs.writeFile(pkgPath, JSON.stringify(pkg, null, indent));
 };
 
 const fixMissingDependencies = async (errors: MissingDep[], cwd: string) => {
-  const cannotAdd: { [key: string]: string[] } = {};
-  const update = new Map();
+  let cannotAdd: { [key: string]: string[] } = {};
+  let update = new Map();
 
+  // This step checks for dependencies that exist at two different versions
+  // within different packages.
   errors.forEach(({ dependency, pkgVersion }) => {
-    if (update.has(dependency)) {
-      if (!cannotAdd.dependency) {
-        cannotAdd[dependency] = [];
-      }
-      const v1 = update.get(dependency);
+    const conflict = update.get(dependency);
+    if (conflict && conflict !== pkgVersion) {
+      let v1 = update.get(dependency);
       cannotAdd[dependency].push(v1, pkgVersion);
       update.delete(dependency);
-    } else if (cannotAdd[dependency]) {
+    } else if (cannotAdd[dependency] && !cannotAdd[dependency].includes(pkgVersion)) {
       cannotAdd[dependency].push(pkgVersion);
     } else {
       update.set(dependency, pkgVersion);
     }
   });
 
-  const rootPkgJsonPath = path.resolve(cwd, "package.json");
-  const rootRaw = await fs.readFile(rootPkgJsonPath, "utf-8");
-  const indent = detectIndent(rootRaw).indent || "  ";
-  const rootPkgJson = JSON.parse(rootRaw);
+  let rootPkgJsonPath = path.resolve(cwd, "package.json");
+  let rootRaw = await fs.readFile(rootPkgJsonPath, "utf-8");
+  let indent = detectIndent(rootRaw).indent || "  ";
+  let rootPkgJson = JSON.parse(rootRaw);
 
   update.forEach((pkgVersion, dependency) => {
     if (!rootPkgJson.dependencies) {
