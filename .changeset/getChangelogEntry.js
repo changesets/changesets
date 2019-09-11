f8103a12d59a5f7bc1ee1d0fd29dca559371ae17
Ben Conolly <ben@thinkmill.com.au>
2019-09-12 08:29:33 +1000

    update our changelog generator function

--- .changeset/getChangelogEntry.js
+++ .changeset/getChangelogEntry.js
@@ -5,27 +5,35 @@ const getReleaseLine = async (changeset, type) => {
   const [firstLine, ...futureLines] = changeset.summary
     .split("\n")
     .map(l => l.trimRight());
-  let { links } = await getInfo({
-    repo: "changesets/changesets",
-    commit: changeset.commit
-  });
-  return `- ${links.commit}${links.pull === null ? "" : ` ${links.pull}`}${
-    links.user === null ? "" : ` Thanks ${links.user}!`
-  } - ${firstLine}\n${futureLines.map(l => `  ${l}`).join("\n")}`;
+
+  if (changeset.commit) {
+    let { links } = await getInfo({
+      repo: "atlassian/changesets",
+      commit: changeset.commit
+    });
+    return `- ${links.commit}${links.pull === null ? "" : ` ${links.pull}`}${
+      links.user === null ? "" : ` Thanks ${links.user}!`
+    } - ${firstLine}\n${futureLines.map(l => `  ${l}`).join("\n")}`;
+  } else {
+    return `- ${firstLine}\n${futureLines.map(l => `  ${l}`).join("\n")}`;
+  }
 };
 
 const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
   if (dependenciesUpdated.length === 0) return "";
 
-  const changesetLinks = changesets.map(
-    changeset => `- Updated dependencies [${changeset.commit}]:`
-  );
+  changesets.map(cs => cs.commit).filter(_ => _);
+
+  const changesetLink = `- Updated dependencies [${changesets
+    .map(cs => cs.commit)
+    .filter(_ => _)
+    .join(", ")}]:`;
 
   const updatedDepenenciesList = dependenciesUpdated.map(
-    dependency => `  - ${dependency.name}@${dependency.version}`
+    dependency => `  - ${dependency.name}@${dependency.newVersion}`
   );
 
-  return [...changesetLinks, ...updatedDepenenciesList].join("\n");
+  return [changesetLink, ...updatedDepenenciesList].join("\n");
 };
 
 module.exports = {
