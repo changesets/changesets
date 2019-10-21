a76a2ce52c8c0a30aa2793f2eca64a99fc846bba
Ajay Narain Mathur <ajaynarainmathur@gmail.com>
2019-10-22 05:07:49 +1100

    remove ts-ignore since we using commonjs modles now

--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -15,32 +15,17 @@ export const temporarilySilenceLogs = () => {
   const originalWarn = logger.warn;
   const originalSuccess = logger.success;
   beforeEach(() => {
-    // @ts-ignore: We need to re-assign function since mocking is per file therefore with mocking this
-    // function will be mocked only for called int his file and not for in the other test files
     logger.error = jest.fn();
-    // @ts-ignore: We need to re-assign function since mocking is per file therefore with mocking this
-    // function will be mocked only for called int his file and not for in the other test files
     logger.info = jest.fn();
-    // @ts-ignore: We need to re-assign function since mocking is per file therefore with mocking this
-    // function will be mocked only for called int his file and not for in the other test files
     logger.log = jest.fn();
-    // @ts-ignore: We need to re-assign function since mocking is per file therefore with mocking this
-    // function will be mocked only for called int his file and not for in the other test files
     logger.warn = jest.fn();
-    // @ts-ignore: We need to re-assign function since mocking is per file therefore with mocking this
-    // function will be mocked only for called int his file and not for in the other test files
     logger.success = jest.fn();
   });
   afterEach(() => {
-    // @ts-ignore restoring the function back to its original implementation
     logger.error = originalError;
-    // @ts-ignore restoring the function back to its original implementation
     logger.info = originalInfo;
-    // @ts-ignore restoring the function back to its original implementation
     logger.log = originalLog;
-    // @ts-ignore restoring the function back to its original implementation
     logger.warn = originalWarn;
-    // @ts-ignore restoring the function back to its original implementation
     logger.success = originalSuccess;
   });
 };
