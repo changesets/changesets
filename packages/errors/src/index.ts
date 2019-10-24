1cdb85c36f4eca8279a4736fa2a5b06831dc30fb
mitchellhamilton <mitchell@hamil.town>
2019-10-25 08:30:22 +1000

    Try a thing

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,15 +1,19 @@
 import ExtendableError from "extendable-error";
 
 export class GitError extends ExtendableError {
-  constructor(public code: number, message: string) {
+  code: number;
+  constructor(code: number, message: string) {
     super(`${message}, exit code: ${code}`);
+    this.code = code;
   }
 }
 
 export class ValidationError extends ExtendableError {}
 
 export class ExitError extends ExtendableError {
-  constructor(public code: number) {
+  code: number;
+  constructor(code: number) {
     super(`The process exited with code: ${code}`);
+    this.code = code;
   }
 }
