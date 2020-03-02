dcf07ea796336aeaa4060cb770a707da880bfbf0
Tarang Hirani <tarang9211@gmail.com>
2020-03-02 17:13:12 +1100

    fixed lint errors

--- packages/cli/src/utils/cli-utilities.ts
+++ packages/cli/src/utils/cli-utilities.ts
@@ -1,6 +1,6 @@
 // @ts-ignore it's not worth writing a TS declaration file in this repo for a tiny module we use once like this
 import termSize from "term-size";
-import { error, prefix, success, log } from "@changesets/logger";
+import { error, prefix, success } from "@changesets/logger";
 import { prompt } from "enquirer";
 
 /* Notes on using inquirer:
