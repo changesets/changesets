2071ef184390604b4c1b1490d3de0d582f24a9e0
Feiyang1 <plane1113@gmail.com>
2020-05-18 18:11:07 -0700

    use Array.prototype.includes

--- packages/assemble-release-plan/src/is-ignored.ts
+++ packages/assemble-release-plan/src/is-ignored.ts
@@ -1,3 +1,3 @@
 export default function isIgnoredPackage(packageName: string, ignored: Readonly<string[]>): boolean {
-    return !!ignored.find(ignoredPackageName => ignoredPackageName === packageName);
+    return ignored.includes(packageName);
 }
