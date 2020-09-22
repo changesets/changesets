99353b33f5b5faeef5e280fbcec079fbd51663a7
Adam Haglund <adam.lj.haglund@gmail.com>
2020-09-22 11:09:09 +0200

    Add `doNotThank` option

--- packages/changelog-github/src/index.ts
+++ packages/changelog-github/src/index.ts
@@ -40,12 +40,23 @@ const changelogFunctions: ChangelogFunctions = {
 
     return [changesetLink, ...updatedDepenenciesList].join("\n");
   },
-  getReleaseLine: async (changeset, type, options) => {
-    if (!options || !options.repo) {
+  getReleaseLine: async (changeset, type, options = {}) => {
+    if (!options.repo) {
       throw new Error(
         'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
       );
     }
+
+    const shouldThank = (user: string) => {
+      // Handle an array of names to not thank
+      if (Array.isArray(options.doNotThank)) {
+        return options.doNotThank.includes(user)
+      }
+      
+      // Should thank if it's null, true, anything except false
+      return options.doNotThank !== false
+    }
+
     const [firstLine, ...futureLines] = changeset.summary
       .split("\n")
       .map(l => l.trimRight());
@@ -56,9 +67,9 @@ const changelogFunctions: ChangelogFunctions = {
         commit: changeset.commit
       });
       return `\n\n- ${links.commit}${
-        links.pull === null ? "" : ` ${links.pull}`
+        links.pull == null ? "" : ` ${links.pull}`
       }${
-        links.user === null ? "" : ` Thanks ${links.user}!`
+        links.user == null && shouldThank(links.user) ? "" : ` Thanks ${links.user}!`
       } - ${firstLine}\n${futureLines.map(l => `  ${l}`).join("\n")}`;
     } else {
       return `\n\n- ${firstLine}\n${futureLines.map(l => `  ${l}`).join("\n")}`;
