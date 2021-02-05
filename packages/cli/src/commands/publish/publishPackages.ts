03b2ade56e4673a10b508495916d291d7fbcdd0f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2021-02-05 10:18:26 +0100

    Slight refactor of publishing limiting (#529)

--- packages/cli/src/commands/publish/publishPackages.ts
+++ packages/cli/src/commands/publish/publishPackages.ts
@@ -5,7 +5,6 @@ import { AccessType } from "@changesets/types";
 import { Package } from "@manypkg/get-packages";
 import { info, warn } from "@changesets/logger";
 import { PreState } from "@changesets/types";
-import pLimit from "p-limit";
 import * as npmUtils from "./npm-utils";
 import { TwoFactorState } from "../../utils/types";
 import isCI from "../../utils/isCI";
@@ -24,9 +23,6 @@ type PublishedResult = {
   newVersion: string;
   published: boolean;
 };
-
-// TODO: make it configurable?
-const npmPublishLimit = pLimit(10);
 
 function getReleaseTag(pkgInfo: PkgInfo, preState?: PreState, tag?: string) {
   if (tag) return tag;
@@ -91,20 +87,15 @@ export default async function publishPackages({
   }
 
   return Promise.all(
-    unpublishedPackagesInfo.map(pkgInfo =>
-      // If there are many packages to be published, it's better to limit the
-      // concurrency to avoid unwanted errors, for example from npm.
-      npmPublishLimit(async () => {
-        let pkg = packagesByName.get(pkgInfo.name)!;
-        const publishedResult = await publishAPackage(
-          pkg,
-          access,
-          twoFactorState,
-          getReleaseTag(pkgInfo, preState, tag)
-        );
-        return publishedResult;
-      })
-    )
+    unpublishedPackagesInfo.map(pkgInfo => {
+      let pkg = packagesByName.get(pkgInfo.name)!;
+      return publishAPackage(
+        pkg,
+        access,
+        twoFactorState,
+        getReleaseTag(pkgInfo, preState, tag)
+      );
+    })
   );
 }
 
