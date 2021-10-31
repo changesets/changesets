2955237c69209a8b6b5fa037a59b08d9e298da11
Tmk <i@tmk.im>
2021-10-31 22:45:00 +0800

    Fix generated undefined in changelog

--- packages/cli/src/changelog/index.ts
+++ packages/cli/src/changelog/index.ts
@@ -32,7 +32,9 @@ const getDependencyReleaseLine = async (
   if (dependenciesUpdated.length === 0) return "";
 
   const changesetLinks = changesets.map(
-    changeset => `- Updated dependencies [${changeset.commit}]`
+    changeset => `- Updated dependencies${
+      changeset.commit ? ` [${changeset.commit}]` : ""
+    }`
   );
 
   const updatedDepenenciesList = dependenciesUpdated.map(
