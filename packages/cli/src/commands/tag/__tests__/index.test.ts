2b49c390a7cf24ce859ac932b432eb6d8f55c98b
Joel Denning <joeldenning@gmail.com>
2021-11-07 10:42:27 -0700

    Implement `changeset tag` and `getAllTags` (#634)
    * Implement changeset tag and getAllTags. Resolves #612.
    
    * Self review
    
    * Fix tests
    
    * Feedback
    
    * Fix formatting
    
    * Support for single package repos.
    
    * Self review
    
    * Remove redundant `.filter(Boolean)`
    
    * tweaks
    
    * tweak frontmatters
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

---
+++ packages/cli/src/commands/tag/__tests__/index.test.ts
@@ -0,0 +1,58 @@
+import fixtures from "fixturez";
+import { temporarilySilenceLogs } from "../../../../../test-utils/src";
+import * as git from "@changesets/git";
+import tag from "../index";
+
+const f = fixtures(__dirname);
+
+jest.mock("@changesets/git");
+
+describe("tag command", () => {
+  temporarilySilenceLogs();
+  let cwd: string;
+
+  describe("workspace project", () => {
+    beforeEach(async () => {
+      cwd = await f.copy("simple-project");
+    });
+
+    it("tags all packages", async () => {
+      (git.getAllTags as jest.Mock).mockReturnValue(new Set());
+
+      expect(git.tag).not.toHaveBeenCalled();
+      await tag(cwd);
+      expect(git.tag).toHaveBeenCalledTimes(2);
+      expect((git.tag as jest.Mock).mock.calls[0][0]).toEqual("pkg-a@1.0.0");
+      expect((git.tag as jest.Mock).mock.calls[1][0]).toEqual("pkg-b@1.0.0");
+    });
+
+    it("skips tags that already exist", async () => {
+      (git.getAllTags as jest.Mock).mockReturnValue(
+        new Set([
+          // pkg-a should not be re-tagged
+          "pkg-a@1.0.0"
+        ])
+      );
+
+      expect(git.tag).not.toHaveBeenCalled();
+      await tag(cwd);
+      expect(git.tag).toHaveBeenCalledTimes(1);
+      expect((git.tag as jest.Mock).mock.calls[0][0]).toEqual("pkg-b@1.0.0");
+    });
+  });
+
+  describe("single package repo", () => {
+    beforeEach(async () => {
+      cwd = await f.copy("root-only");
+    });
+
+    it("uses a simplified version-only tag", async () => {
+      (git.getAllTags as jest.Mock).mockReturnValue(new Set());
+
+      expect(git.tag).not.toHaveBeenCalled();
+      await tag(cwd);
+      expect(git.tag).toHaveBeenCalledTimes(1);
+      expect((git.tag as jest.Mock).mock.calls[0][0]).toEqual("v1.0.0");
+    });
+  });
+});
