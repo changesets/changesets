6d37ea52a03a1cde88160f18da2dbc406c89cba8
Jake Ginnivan <jake@ginnivan.net>
2022-02-08 17:31:28 +0800

    Export tagExists function

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -281,14 +281,12 @@ export async function getChangedPackagesSinceRef({
   );
 }
 
-async function tagExists(tagStr: string) {
+export async function tagExists(tagStr: string) {
   const gitCmd = await spawn("git", ["tag", "-l", tagStr]);
   const output = gitCmd.stdout.toString().trim();
   const tagExists = !!output;
   return tagExists;
 }
-
-  tagExists,
 
 export async function getCurrentCommitId({
   cwd,
