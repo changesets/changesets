52f8b6fef8298ad35ff4dfed9dda1483ec44d087
Brian Espinosa <b@bje.co>
2022-05-08 17:02:38 -0700

    Tests passing for dir, packageJson.name

--- packages/get-workspaces/src/index.test.ts
+++ packages/get-workspaces/src/index.test.ts
@@ -1,34 +1,37 @@
 import fixtures from "fixturez";
+import path from "path";
 
 import { getWorkspaces, Packages } from "./index";
 
 const f = fixtures(__dirname);
 
+// BAD CODE!!! -- Should not reach out of a monorepo package. This is only for a test.
+const FIXTURE_PROJECTS: string[][] = Object.entries(
+  require("../../../__fixtures__/nx-workspace-base/workspace.json").projects
+).filter(([name]) => !name.startsWith("my-app"));
+
 describe("getWorkspaces", () => {
   let cwd: string;
+  let workspace: Packages;
+
   beforeEach(async () => {
+    // cwd = await f.copy("nx-workspace-base");
     cwd = await f.copy("nx-workspace-base");
+    workspace = await getWorkspaces(cwd);
+  });
+
+  it("should detect an NX workspace tool if NX is configured, and no other tool is defined", async () => {
+    // The tool should show as "nx"
+    expect(workspace.tool).toEqual("nx");
   });
 
   it("should detect all NX workspace packages", async () => {
-    let packages: Packages;
-
-    try {
-      packages = await getWorkspaces(cwd);
-    } catch (e) {
-      // ignore errors.
-    }
-
-    // The tool should show as "nx"
-    expect(packages.tool).toEqual("nx");
-    // There should be 4 packages
-    expect(packages.packages.length).toEqual(4);
-    // They should look like this
-    expect(packages.packages).toEqual([
-      "PackageObjectA",
-      "PackageObjectB",
-      "PackageObjectC",
-      "PackageObjectD"
-    ]);
+    // There should be 2 packages
+    expect(workspace.packages.length).toEqual(FIXTURE_PROJECTS.length);
+    // Their dir and name should be:
+    FIXTURE_PROJECTS.map(([name, projectPath], i) => {
+      expect(workspace.packages[i].dir).toEqual(path.resolve(cwd, projectPath));
+      expect(workspace.packages[i].packageJson.name).toEqual("@myorg/" + name);
+    });
   });
 });
--- packages/get-workspaces/src/index.ts
+++ packages/get-workspaces/src/index.ts
@@ -1,5 +1,6 @@
 import fs from "fs-extra";
 import path from "path";
+import { Workspaces } from "@nrwl/devkit";
 import { getPackages, Package, Tool } from "@manypkg/get-packages";
 
 type WorkspaceTool = Tool | "nx";
@@ -9,6 +10,22 @@ export type Packages = {
   tool: WorkspaceTool;
   packages: Package[];
   root: Package;
+};
+
+const getValuesFromPackageFile = async (dir: string): Promise<Package> => {
+  const packageJsonPath = path.resolve(dir, "package.json");
+
+  const data = await fs.promises.readFile(packageJsonPath);
+  const { name, version, dependencies } = JSON.parse(data.toString());
+
+  return {
+    dir,
+    packageJson: {
+      dependencies,
+      name,
+      version
+    }
+  };
 };
 
 export const getWorkspaces = async (cwd: string): Promise<Packages> => {
@@ -24,14 +41,32 @@ export const getWorkspaces = async (cwd: string): Promise<Packages> => {
   const isNX =
     manyTool === "root" && fs.existsSync(path.resolve(cwd, "nx.json"));
 
-  if (isNX) {
-    // Do the NX devkit commands to get packages here and map over the
-    // output to create Package typed objects.
+  if (!isNX) {
+    return {
+      tool: manyTool,
+      packages: manyPackages,
+      root
+    };
+  }
+
+  // We are using native NX tools to read packages available instead of using
+  // workspace.json alone as there are multiple ways to configure NX workspaces
+  const { projects } = new Workspaces(cwd).readWorkspaceConfiguration();
+
+  const projectRoots = Object.values(projects).map(({ root }) => root);
+
+  let nxPackages = [];
+  for (let projectRoot of projectRoots) {
+    const packageDir = path.resolve(cwd, projectRoot);
+    try {
+      const data = await getValuesFromPackageFile(packageDir);
+      nxPackages.push(data);
+    } catch (e) {}
   }
 
   return {
-    tool: isNX ? "nx" : manyTool,
-    packages: manyPackages,
+    tool: "nx",
+    packages: nxPackages,
     root
   };
 };
