28a939348a05e9fceb42383d0bb8fa3ed13f0bcf
George Reith <georeith@gmail.com>
2023-06-14 17:38:58 +0100

    test

--- packages/cli/src/run.test.ts
+++ packages/cli/src/run.test.ts
@@ -65,6 +65,36 @@ describe("cli", () => {
       );
     });
 
+    it("should not throw if devDependents of ignored packages are not explicitly listed in the ignore array", async () => {
+      const cwd = await testdir({
+        "package.json": JSON.stringify({
+          private: true,
+          workspaces: ["packages/*"],
+        }),
+        "packages/pkg-a/package.json": JSON.stringify({
+          name: "pkg-a",
+          version: "1.0.0",
+          devDependencies: {
+            "pkg-b": "1.0.0",
+          },
+        }),
+        "packages/pkg-b/package.json": JSON.stringify({
+          name: "pkg-b",
+          version: "1.0.0",
+        }),
+        ".changeset/config.json": JSON.stringify({}),
+      });
+      try {
+        await run(["version"], { ignore: ["pkg-b"] }, cwd);
+      } catch (e) {
+        // ignore the error. We just want to validate the error message
+      }
+
+      const loggerErrorCalls = (error as any).mock.calls;
+      expect(loggerErrorCalls.length).toEqual(0);
+    });
+
+
     it("should throw if `--ignore` flag is used while ignore array is also defined in the config file ", async () => {
       const cwd = await testdir({
         "package.json": JSON.stringify({
