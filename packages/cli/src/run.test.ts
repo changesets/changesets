7986a091e5b6c300daef2ca33e8bce34076f402b
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-05-23 15:04:26 -0700

    one last test

--- packages/cli/src/run.test.ts
+++ packages/cli/src/run.test.ts
@@ -65,6 +65,41 @@ describe("cli", () => {
       );
     });
 
+    it("should not throw if dependents of unversioned private packages are not explicitly listed in the ignore array", async () => {
+      const cwd = await testdir({
+        "package.json": JSON.stringify({
+          private: true,
+          workspaces: ["packages/*"],
+        }),
+        "packages/pkg-a/package.json": JSON.stringify({
+          name: "pkg-a",
+          version: "1.0.0",
+          private: true,
+          dependencies: {
+            "pkg-b": "1.0.0",
+          },
+        }),
+        "packages/pkg-b/package.json": JSON.stringify({
+          name: "pkg-b",
+          version: "1.0.0",
+        }),
+        ".changeset/config.json": JSON.stringify({
+          privatePackages: {
+            tag: false,
+            version: false,
+          },
+        }),
+      });
+      try {
+        await run(["version"], { ignore: ["pkg-b"] }, cwd);
+      } catch (e) {
+        // ignore the error. We just want to validate the error message
+      }
+
+      const loggerErrorCalls = (error as any).mock.calls;
+      expect(loggerErrorCalls.length).toEqual(0);
+    });
+
     it("should throw if `--ignore` flag is used while ignore array is also defined in the config file ", async () => {
       const cwd = await testdir({
         "package.json": JSON.stringify({
