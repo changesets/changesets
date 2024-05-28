5d70fb75fc4864085d807265fc560954d9985ce2
Tyler Butler <tyler@tylerbutler.com>
2024-05-27 18:36:10 -0700

    comments

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -109,10 +109,10 @@ export let read = async (cwd: string, packages: Packages): Promise<Config> => {
   // Dynamically load cosmiconfig only if we proceed down this code path.
   const { cosmiconfig } = await import("cosmiconfig");
   const configExplorer = cosmiconfig("changesets", {
-    // Only check for CommonJS config; JSON and JSONC formats are loaded by existing logic above. The .mjs file format
-    // is exlcuded because it seems to break tests. The .js extension is excluded because there is code elsewhere that
-    // assumes such a file is a V1 changesets config file. To limit the size of this change, only .cjs config files will
-    // be loaded. A future change could add to this list and fully remove code related to the V1 config format.
+    // Only check for CommonJS config; JSON is loaded by existing logic above. The .mjs file format is exlcuded because
+    // it seems to break tests. The .js extension is excluded because there is code elsewhere that assumes such a file
+    // is a V1 changesets config file. To limit the size of this change, only .cjs config files will be loaded. A future
+    // change could add to this list and fully remove code related to the V1 config format.
     searchPlaces: [".changeset/config.cjs"],
 
     // Don't look up the folder tree for additional config files.
