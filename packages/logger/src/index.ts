d7ff21525f193d7f99ab66dc7991ad1be4f174f4
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-07-15 08:51:39 -0700

    chore(logger): replace chalk with picocolors

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,4 +1,4 @@
-import chalk from "chalk";
+import pc from "picocolors";
 import util from "util";
 
 export let prefix = "🦋 ";
@@ -16,11 +16,11 @@ function format(args: Array<any>, customPrefix?: string) {
 }
 
 export function error(...args: Array<any>) {
-  console.error(format(args, chalk.red("error")));
+  console.error(format(args, pc.red("error")));
 }
 
 export function info(...args: Array<any>) {
-  console.info(format(args, chalk.cyan("info")));
+  console.info(format(args, pc.cyan("info")));
 }
 
 export function log(...args: Array<any>) {
@@ -28,9 +28,9 @@ export function log(...args: Array<any>) {
 }
 
 export function success(...args: Array<any>) {
-  console.log(format(args, chalk.green("success")));
+  console.log(format(args, pc.green("success")));
 }
 
 export function warn(...args: Array<any>) {
-  console.warn(format(args, chalk.yellow("warn")));
+  console.warn(format(args, pc.yellow("warn")));
 }
