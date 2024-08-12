8547536843753445763a042a66abaa705ffe056e
with-heart <with.heart+git@pm.me>
2024-08-12 15:00:08 -0400

    Revert "clean up unused files" and add knip workspace for them
    This reverts commit b61bd7670fac425a941377ff0cd9b5a1b42fd759.

---
+++ packages/apply-release-plan/src/test-utils/simple-get-changelog-entry.ts
@@ -0,0 +1,4 @@
+// We are doing it here to avoide adding a circular dependency and as this is only used in testing.
+// This is wicked, and please don't copy us.
+// eslint-disable-next-line import/no-extraneous-dependencies
+export { default } from "@changesets/cli/changelog";
