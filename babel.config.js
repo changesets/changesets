6524f6ad61b7a92c058d0e24fbdde28c19046a68
bluwy <bjornlu.dev@gmail.com>
2024-10-05 14:56:10 +0800

    Remove `@babel/preset-env`

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      "@babel/preset-env",
-      {
-        targets: { node: 18 },
-      },
-    ],
-  ],
   overrides: [{ test: "**/*.ts", presets: ["@babel/preset-typescript"] }],
 };
