2054b7dcde52d50e737afff7336dddec881f5438
bluwy <bjornlu.dev@gmail.com>
2024-10-05 15:01:55 +0800

    Revert "Remove `@babel/preset-env`"
    This reverts commit 6524f6ad61b7a92c058d0e24fbdde28c19046a68.

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: 18 },
+      },
+    ],
+  ],
   overrides: [{ test: "**/*.ts", presets: ["@babel/preset-typescript"] }],
 };
