4d330a9478c8b5ca373023926d6ad6d233473042
Mateusz Burzyński <mateuszburzynski@gmail.com>
2024-10-09 11:16:40 +0200

    Update packages/read/src/index.ts

--- packages/read/src/index.ts
+++ packages/read/src/index.ts
@@ -48,7 +48,7 @@ export default async function getChangesets(
     (file) =>
       !file.startsWith(".") &&
       file.endsWith(".md") &&
-      file.toLowerCase() !== "readme.md"
+      !/^README\.md$/i.test(file)
   );
 
   const changesetContents = changesets.map(async (file) => {
