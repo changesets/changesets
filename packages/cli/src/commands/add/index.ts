dae6ab56a6e02d3442d3d152d56d139a7443932d
Gautier Ben Aïm <48261497+GauBen@users.noreply.github.com>
2024-10-25 15:59:58 +0200

    fix: prevent crash when trying to version the root of the monorepo

--- packages/cli/src/commands/add/index.ts
+++ packages/cli/src/commands/add/index.ts
@@ -53,6 +53,11 @@ export default async function add(
       releases: [],
       summary: ``,
     };
+  } else if (versionablePackages.length === 0) {
+    warn("This workspace does not contain any packages that can be versioned");
+    warn(`If you intend to version the root package, add ${pc.green('"."')} to the list of workspaces`);
+    warn(`See ${pc.blue('https://github.com/changesets/changesets/issues/1137')} for more details`);
+    newChangeset = { confirmed: false };
   } else {
     const changedPackagesNames = (
       await getVersionableChangedPackages(config, {
