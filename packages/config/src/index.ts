f0270f69793ddb6865f2510d334864b093cb10e3
An Phi <aphi@skidmore.edu>
2024-11-29 04:26:54 -0500

    relax parameters for read() (#1517)
    * relax signature of read() and parse()
    
    * cleanup
    
    * fix bug
    
    * fix lint
    
    * Update packages/config/src/index.ts
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>
    
    * add test
    
    * Update packages/config/src/index.test.ts
    
    * Create wise-kangaroos-cheer.md
    
    ---------
    
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import micromatch from "micromatch";
 import { ValidationError } from "@changesets/errors";
 import { warn } from "@changesets/logger";
-import { Packages } from "@manypkg/get-packages";
+import { Packages, getPackages } from "@manypkg/get-packages";
 import {
   Config,
   WrittenConfig,
@@ -91,7 +91,8 @@ function isArray<T>(
   return Array.isArray(arg);
 }
 
-export let read = async (cwd: string, packages: Packages) => {
+export let read = async (cwd: string, packages?: Packages) => {
+  packages ??= await getPackages(cwd);
   let json = await fs.readJSON(path.join(cwd, ".changeset", "config.json"));
   return parse(json, packages);
 };
