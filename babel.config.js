385dd3e5a72280ef62d99f63cbf911c069f05048
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-02-22 15:37:05 +0100

    tweak things

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,3 @@
-const plugins = [];
-
-if (process.env.NODE_ENV === "test") {
-  plugins.push(function ({ types: t }) {
-    return {
-      visitor: {
-        MetaProperty(path) {
-          const parentPath = path.parentPath;
-          if (
-            !parentPath.isMemberExpression() ||
-            !parentPath.get("property").isIdentifier({ name: "url" })
-          ) {
-            return;
-          }
-          parentPath.replaceWith(t.identifier("__filename"));
-        },
-      },
-    };
-  });
-}
 module.exports = {
   presets: [
     [
@@ -27,6 +7,5 @@ module.exports = {
       },
     ],
   ],
-  plugins,
   overrides: [{ test: "**/*.ts", presets: ["@babel/preset-typescript"] }],
 };
