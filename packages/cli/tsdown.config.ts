3d9e40422136cfa1e39e0c878bca27d652f1194c
bq <adam@haglund.dev>
2025-12-11 14:11:12 +0100

    use tsdown in build scripts, update exports

--- packages/cli/tsdown.config.ts
+++ packages/cli/tsdown.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from "tsdown/config";
 import { baseConfig } from "../../tsdown.config.ts";
 
-export default defineConfig(baseConfig);
+export default defineConfig({
+  ...baseConfig,
+  entry: ["src/index.ts", "src/changelog.ts", "src/commit/index.ts"],
+});
