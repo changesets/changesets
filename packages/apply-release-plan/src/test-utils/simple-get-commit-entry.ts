295227f99af462bb4e2764de0e1e63874b19ec7e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-05-04 18:27:55 +0200

    fix commit too

--- packages/apply-release-plan/src/test-utils/simple-get-commit-entry.ts
+++ packages/apply-release-plan/src/test-utils/simple-get-commit-entry.ts
@@ -1,3 +1,16 @@
 // We are doing it here to avoide adding a circular dependency and as this is only used in testing.
 // This is wicked, and please don't copy us.
-export { default } from "@changesets/cli/commit";
+
+import path from "node:path";
+
+const commitPath = path.resolve(
+  import.meta.dirname,
+  "..",
+  "..",
+  "..",
+  "cli",
+  "dist",
+  "changesets-cli-commit.js",
+);
+const commitModule = await import(commitPath);
+export default commitModule.default;
