c02d9c5620dd31b0a648a549d11c3fd5c5c3e768
bq <adam@haglund.dev>
2026-05-06 20:32:19 +0200

    remove release-utils package

--- packages/release-utils/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { readChangesetState } from "./readChangesetState.ts";
-export { runPublish as publish, runVersion as version } from "./run.ts";
-export {
-  getChangelogEntry,
-  BumpLevels,
-  sortChangelogEntries,
-} from "./utils.ts";
