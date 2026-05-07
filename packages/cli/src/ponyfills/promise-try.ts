94fb471dc82fbf694385e7be09900335e9f4e282
bq <adam@haglund.dev>
2026-05-07 13:17:48 +0200

    improve name of polyfill

--- packages/cli/src/ponyfills/promise-try.ts
+++ packages/cli/src/ponyfills/promise-try.ts
@@ -1,5 +1,5 @@
 // TODO [engine:node@>=23]: remove when supported by minimum node version
-const polyfill = <T>(fn: () => Promise<T> | T): Promise<T> =>
+const promiseTryPolyfill = <T>(fn: () => Promise<T> | T): Promise<T> =>
   new Promise((resolve) => resolve(fn()));
 
-export const promiseTry = Promise.try ?? polyfill;
+export const promiseTry = Promise.try ?? promiseTryPolyfill;
