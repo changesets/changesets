0e53ab89d84752ff5c53bef97b067849cdf0bd22
bq <adam@haglund.dev>
2026-05-07 13:39:50 +0200

    fix type and impl of polyfill
    it was missing that you can pass args into `try`

--- packages/cli/src/ponyfills/promise-try.ts
+++ packages/cli/src/ponyfills/promise-try.ts
@@ -1,6 +1,9 @@
 // TODO [engine:node@>=23]: remove when supported by minimum node version
-const promiseTryPolyfill = <T>(fn: () => Promise<T> | T): Promise<T> =>
-  new Promise((resolve) => resolve(fn()));
+const promiseTryPolyfill = <R, Args extends unknown[]>(
+  fn: (...args: Args) => Promise<R> | R,
+  ...args: Args
+): Promise<Awaited<R>> =>
+  new Promise((resolve) => resolve(fn(...args) as Awaited<R>));
 
 export const promiseTry =
   "try" in Promise ? Promise.try.bind(Promise) : promiseTryPolyfill;
