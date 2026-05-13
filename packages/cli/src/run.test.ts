1051af138c5e7950e185aaabd7ae778998f27ad3
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-05-13 09:27:03 +0200

    move tests

--- packages/cli/src/run.test.ts
+++ packages/cli/src/run.test.ts
@@ -61,65 +61,6 @@ describe("cli", () => {
   });
 
   describe("version", () => {
-    it("should validate package name passed in from --ignore flag", async () => {
-      const cwd = await testdir({
-        "package.json": JSON.stringify({
-          private: true,
-          workspaces: ["packages/*"],
-        }),
-        "package-lock.json": "",
-        "packages/pkg-a/package.json": JSON.stringify({
-          name: "pkg-a",
-          version: "1.0.0",
-        }),
-        ".changeset/config.json": JSON.stringify({}),
-      });
-      try {
-        await run(["version"], { ignore: "pkg-c" }, cwd);
-      } catch {
-        // ignore errors. We just want to validate the error message
-      }
-
-      expect(mockedLogger.error).toHaveBeenCalledOnce();
-      const arg = mockedLogger.error.mock.calls[0][0];
-      expect(stripVTControlCharacters(arg)).toEqual(
-        `The package pkg-c is passed to the \`--ignore\` option but it is not found in the project. You may have misspelled the package name.`,
-      );
-    });
-
-    it("should throw if dependents of ignored packages are not explicitly listed in the ignore array", async () => {
-      const cwd = await testdir({
-        "package.json": JSON.stringify({
-          private: true,
-          workspaces: ["packages/*"],
-        }),
-        "package-lock.json": "",
-        "packages/pkg-a/package.json": JSON.stringify({
-          name: "pkg-a",
-          version: "1.0.0",
-          dependencies: {
-            "pkg-b": "1.0.0",
-          },
-        }),
-        "packages/pkg-b/package.json": JSON.stringify({
-          name: "pkg-b",
-          version: "1.0.0",
-        }),
-        ".changeset/config.json": JSON.stringify({}),
-      });
-      try {
-        await run(["version"], { ignore: ["pkg-b"] }, cwd);
-      } catch {
-        // ignore the error. We just want to validate the error message
-      }
-
-      expect(mockedLogger.error).toHaveBeenCalledOnce();
-      const arg = mockedLogger.error.mock.calls[0][0];
-      expect(stripVTControlCharacters(arg)).toEqual(
-        `The package pkg-a depends on the skipped package pkg-b (either by \`ignore\` option or by \`privatePackages.version\`), but pkg-a is not being skipped. Please pass pkg-a to the --ignore flag.`,
-      );
-    });
-
     it("should not throw if dependents of unversioned private packages are not explicitly listed by the ignore flag", async () => {
       const cwd = await testdir({
         "package.json": JSON.stringify({
@@ -242,41 +183,6 @@ describe("cli", () => {
       await run(["version"], { ignore: ["pkg-b"] }, cwd);
 
       expect(mockedLogger.error).not.toHaveBeenCalled();
-    });
-
-    it("should throw if `--ignore` flag is used while ignore array is also defined in the config file", async () => {
-      const cwd = await testdir({
-        "package.json": JSON.stringify({
-          private: true,
-          workspaces: ["packages/*"],
-        }),
-        "package-lock.json": "",
-        "packages/pkg-a/package.json": JSON.stringify({
-          name: "pkg-a",
-          version: "1.0.0",
-          dependencies: {
-            "pkg-b": "1.0.0",
-          },
-        }),
-        "packages/pkg-b/package.json": JSON.stringify({
-          name: "pkg-b",
-          version: "1.0.0",
-        }),
-        ".changeset/config.json": JSON.stringify({
-          ignore: ["pkg-a"],
-        }),
-      });
-      try {
-        await run(["version"], { ignore: "pkg-b" }, cwd);
-      } catch {
-        // ignore errors. We just want to validate the error message
-      }
-
-      expect(mockedLogger.error).toHaveBeenCalledOnce();
-      const arg = mockedLogger.error.mock.calls[0][0];
-      expect(stripVTControlCharacters(arg)).toEqual(
-        `It looks like you are trying to use the \`--ignore\` option while ignore is defined in the config file. This is currently not allowed, you can only use one of them at a time.`,
-      );
     });
 
     it("should not throw if `prettier: false` is configured", async () => {
