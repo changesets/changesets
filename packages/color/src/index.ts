710bd0997570d57c280e8c0fc0ffc660abc10e70
Willow (GHOST) <ghostdevbusiness@gmail.com>
2026-05-13 06:06:43 -0700

    Update packages/color/src/index.ts
    Co-authored-by: Mateusz Burzyński <mateuszburzynski@gmail.com>

--- packages/color/src/index.ts
+++ packages/color/src/index.ts
@@ -7,7 +7,8 @@ type Color = Exclude<Parameters<typeof styleText>[0], Array<unknown>>;
 type ColorProxy = Record<Color, (text: string) => string>;
 
 export default new Proxy({} as ColorProxy, {
-  get(_, color: Color) {
-    return (text: string) => styleText(color, text);
+  get(target, color: Color) {
+    target[color] ??= (text) => styleText(color, text)
+    return target[color];
   },
 });
