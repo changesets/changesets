c60ca1632a08eaa2b8ed3af074f05bc3dc14cca8
bluwy <bjornlu.dev@gmail.com>
2026-05-15 14:36:24 +0800

    Refactor consistent pattern for `@changesets/changelog-git` and `@changesets/cli/commit`

--- packages/cli/src/commit/index.ts
+++ packages/cli/src/commit/index.ts
@@ -1,45 +1,31 @@
-import type {
-  Changeset,
-  CommitFunctions,
-  ReleasePlan,
-} from "@changesets/types";
+import type { CommitFunctions } from "@changesets/types";
 
 type SkipCI = boolean | "add" | "version";
 
-const getAddMessage: CommitFunctions["getAddMessage"] = (
-  changeset: Changeset,
-  options: { skipCI?: SkipCI } | null,
-): string => {
-  const skipCI = options?.skipCI === "add" || options?.skipCI === true;
-  const skipMsg = skipCI ? `\n\n[skip ci]\n` : "";
-  return `docs(changeset): ${changeset.summary}${skipMsg}`;
-};
+const commitFunctions: CommitFunctions = {
+  getAddMessage: (changeset, options: { skipCI?: SkipCI } | null) => {
+    const skipCI = options?.skipCI === "add" || options?.skipCI === true;
+    const skipMsg = skipCI ? `\n\n[skip ci]\n` : "";
+    return `docs(changeset): ${changeset.summary}${skipMsg}`;
+  },
+  getVersionMessage: (releasePlan, options: { skipCI?: SkipCI } | null) => {
+    const skipCI = options?.skipCI === "version" || options?.skipCI === true;
+    const publishableReleases = releasePlan.releases.filter(
+      (release) => release.type !== "none",
+    );
+    const numPackagesReleased = publishableReleases.length;
 
-const getVersionMessage: CommitFunctions["getVersionMessage"] = (
-  releasePlan: ReleasePlan,
-  options: { skipCI?: SkipCI } | null,
-): string => {
-  const skipCI = options?.skipCI === "version" || options?.skipCI === true;
-  const publishableReleases = releasePlan.releases.filter(
-    (release) => release.type !== "none",
-  );
-  const numPackagesReleased = publishableReleases.length;
+    const releasesLines = publishableReleases
+      .map((release) => `  ${release.name}@${release.newVersion}`)
+      .join("\n");
 
-  const releasesLines = publishableReleases
-    .map((release) => `  ${release.name}@${release.newVersion}`)
-    .join("\n");
-
-  return `RELEASING: Releasing ${numPackagesReleased} package(s)
+    return `RELEASING: Releasing ${numPackagesReleased} package(s)
 
 Releases:
 ${releasesLines}
 ${skipCI ? `\n[skip ci]\n` : ""}`;
-};
-
-const defaultCommitFunctions: Required<CommitFunctions> = {
-  getAddMessage,
-  getVersionMessage,
+  },
 };
 
 // CommitFunctions require a default export
-export default defaultCommitFunctions;
+export default commitFunctions;
