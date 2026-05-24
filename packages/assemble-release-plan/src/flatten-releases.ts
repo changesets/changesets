0a31e54af7d54f593c8285bd83ec52e4bea59357
bq <adam@haglund.dev>
2026-05-24 11:52:14 +0200

    refactor `flattenReleases` to be easier to read and more performant

--- packages/assemble-release-plan/src/flatten-releases.ts
+++ packages/assemble-release-plan/src/flatten-releases.ts
@@ -1,11 +1,28 @@
-// This function takes in changesets and returns one release per
-// package listed in the changesets
-
 import { shouldSkipPackage } from "@changesets/should-skip-package";
-import type { Config, NewChangeset, Package } from "@changesets/types";
+import type {
+  Config,
+  NewChangeset,
+  Package,
+  VersionType,
+} from "@changesets/types";
 import type { InternalRelease } from "./types.ts";
 import { mapGetOrThrowInternal } from "./utils.ts";
 
+const changeTypes = {
+  major: 3,
+  minor: 2,
+  patch: 1,
+  none: 0,
+} as const;
+
+const changeTypeIsBiggerThanPrevious = (
+  type: VersionType,
+  previous: VersionType,
+) => changeTypes[type] > changeTypes[previous];
+
+/**
+ * Flattens a list of changesets into a package->release-type map
+ */
 export function flattenReleases(
   changesets: NewChangeset[],
   packagesByName: Map<string, Package>,
@@ -13,52 +30,48 @@ export function flattenReleases(
 ): Map<string, InternalRelease> {
   const releases: Map<string, InternalRelease> = new Map();
 
-  changesets.forEach((changeset) => {
-    changeset.releases
+  // Iterate each changeset and its affected packages (`releases`)
+  for (const changeset of changesets) {
+    for (const { name, type } of changeset.releases) {
+      const pkg = mapGetOrThrowInternal(
+        packagesByName,
+        name,
+        `Couldn't find package named "${name}" listed in changeset "${changeset.id}"`,
+      );
+
       // Filter out skipped packages because they should not trigger a release
       // If their dependencies need updates, they will be added to releases by `determineDependents()` with release type `none`
-      .filter(({ name }) => {
-        const pkg = mapGetOrThrowInternal(
-          packagesByName,
-          name,
-          `Couldn't find package named "${name}" listed in changeset "${changeset.id}"`,
-        );
-        return !shouldSkipPackage(pkg, {
-          ignore: config.ignore,
-          allowPrivatePackages: config.privatePackages.version,
-        });
-      })
-      .forEach(({ name, type }) => {
-        const pkg = mapGetOrThrowInternal(
-          packagesByName,
-          name,
-          `Couldn't find package named "${name}" listed in changeset "${changeset.id}"`,
-        );
-        let release = releases.get(name);
-        if (!release) {
-          release = {
-            name,
-            type,
-            oldVersion: pkg.packageJson.version,
-            changesets: [changeset.id],
-          };
-        } else {
-          if (
-            type === "major" ||
-            ((release.type === "patch" || release.type === "none") &&
-              (type === "minor" || type === "patch"))
-          ) {
-            release.type = type;
-          }
-          // Check whether the bumpType will change
-          // If the bumpType has changed recalc newVersion
-          // push new changeset to releases
-          release.changesets.push(changeset.id);
-        }
-
-        releases.set(name, release);
+      const packageIsSkipped = shouldSkipPackage(pkg, {
+        ignore: config.ignore,
+        allowPrivatePackages: config.privatePackages.version,
       });
-  });
+      if (packageIsSkipped) continue;
+
+      const release = releases.get(name);
+
+      if (release == null) {
+        releases.set(name, {
+          name,
+          type,
+          oldVersion: pkg.packageJson.version,
+          changesets: [changeset.id],
+        });
+
+        continue;
+      }
+
+      if (changeTypeIsBiggerThanPrevious(type, release.type)) {
+        release.type = type;
+      }
+
+      // Check whether the bumpType will change
+      // If the bumpType has changed recalc newVersion
+      // push new changeset to releases
+      release.changesets.push(changeset.id);
+
+      releases.set(name, release);
+    }
+  }
 
   return releases;
 }
