a0febcf3c60fe2c5db2a9daeea60306405f3f869
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-06-16 08:56:25 +0200

    fmt

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -106,7 +106,10 @@ cli
 
 cli
   .command("publish-plan", "Show packages that are ready to publish or tag")
-  .option("-o, --output <file>", "Output the publish plan as JSON to a file [experimental]")
+  .option(
+    "-o, --output <file>",
+    "Output the publish plan as JSON to a file [experimental]",
+  )
   .action(async (options) => {
     normalizeOptions(options);
     const { publishPlan } = await import("./commands/publish-plan/index.ts");
