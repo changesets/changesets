c76ddc724d66c4efe31b4273e9e6c75718a523b7
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-06-20 17:56:48 +0200

    refactor: bluwy's suggestions
    Co-authored-by: Bjorn Lu <34116392+bluwy@users.noreply.github.com>

--- packages/cli/src/commands/init/index.ts
+++ packages/cli/src/commands/init/index.ts
@@ -21,32 +21,20 @@ export type AccessType = "public" | "restricted";
 async function getInteractiveConfig() {
   const config = { ...defaultWrittenConfig };
 
-  const changelogChoice = await cli.askList(
-    "Which changelog entry generator should be used?",
-    [
-      {
-        label: "Standard changelogs",
-        value: "@changesets/cli/changelog",
-      },
-      {
-        label: "Changelogs with GitHub info",
-        value: "@changesets/changelog-github",
-      },
-      {
-        label: "Changelogs with commit info",
-        value: "@changesets/changelog-git",
-      },
-    ],
+  const useGithub = await cli.askConfirm(
+    "Should the GitHub integration be used for changelogs?",
+    false,
   );
 
-  if (changelogChoice === "@changesets/changelog-github") {
+  if (useGithub) {
+    const changelogChoice = "@changesets/changelog-github";
     const repo = await cli.askQuestion(
       "What is the GitHub repository? (e.g. org/repo)",
       { placeholder: "org/repo", notEmpty: true },
     );
     config.changelog = [changelogChoice, { repo }];
   } else {
-    config.changelog = changelogChoice;
+    config.changelog = "@changesets/cli/changelog";
   }
 
   config.commit = await cli.askConfirm(
@@ -55,10 +43,10 @@ async function getInteractiveConfig() {
   );
 
   config.access = (await cli.askList(
-    "Should packages be published publicly or privately?",
+    "Should packages be published publicly or privately by default?",
     [
-      { label: "Privately", value: "restricted" },
-      { label: "Publicly", value: "public" },
+      { label: "Private", value: "restricted" },
+      { label: "Public", value: "public" },
     ],
   )) as AccessType | "private";
 
@@ -78,65 +66,41 @@ export async function init(options?: InitOptions): Promise<void> {
   const packages = await getPackages(cwd);
   const changesetBase = path.resolve(packages.rootDir, ".changeset");
 
-  if (existsSync(changesetBase)) {
-    if (!existsSync(path.join(changesetBase, "config.json"))) {
-      log.info(
-        `A new ${c.blue("Changeset")} configuration is being initialized...\n`,
-      );
+  if (existsSync(path.join(changesetBase, "config.json"))) {
+    log.success(
+      `
+${c.green("Changesets")} has already been initialized.
+Changeset commands can be run with no problems.
+      `.trim(),
+    );
+    return;
+  }
 
-      const newConfigStr = await getInteractiveConfig();
+  log.info(
+    `A new ${c.blue("Changeset")} configuration is being initialized...\n`,
+  );
 
-      await fs.writeFile(
-        path.resolve(changesetBase, "config.json"),
-        newConfigStr,
-      );
+  if (!existsSync(changesetBase)) {
+    await fs.mkdir(changesetBase, { recursive: true });
+  }
 
-      if (!existsSync(path.join(changesetBase, "README.md"))) {
-        await fs.copyFile(
-          path.resolve(pkgPath, "./default-files/README.md"),
-          path.resolve(changesetBase, "README.md"),
-        );
-      }
+  const newConfigStr = await getInteractiveConfig();
 
-      log.success(
-        `
+  await fs.writeFile(path.resolve(changesetBase, "config.json"), newConfigStr);
+
+  if (!existsSync(path.join(changesetBase, "README.md"))) {
+    await fs.copyFile(
+      path.resolve(pkgPath, "./default-files/README.md"),
+      path.resolve(changesetBase, "README.md"),
+    );
+  }
+
+  log.success(
+    `
 ${c.green("Changesets")} initialization is complete.
 The ${c.blue(".changeset")} folder has been updated with:
 - ${c.blue("config.json")} (customized based on the provided preferences)
 - ${c.blue("README.md")} (a quick guide for using changesets)
-
-Versioning and publishing can now be managed!
-        `.trim(),
-      );
-    } else {
-      log.success(
-        `
-${c.green("Changesets")} has already been initialized.
-Changeset commands can be run with no problems.
-        `.trim(),
-      );
-    }
-  } else {
-    await fs.cp(path.resolve(pkgPath, "./default-files"), changesetBase, {
-      recursive: true,
-    });
-
-    const newConfigStr = await getInteractiveConfig();
-
-    await fs.writeFile(
-      path.resolve(changesetBase, "config.json"),
-      newConfigStr,
-    );
-
-    log.success(
-      `
-${c.green("Changesets")} initialization is complete.
-The ${c.blue(".changeset")} folder has been successfully created with:
-- ${c.blue("config.json")} (customized based on the provided preferences)
-- ${c.blue("README.md")} (a quick guide for using changesets)
-
-Versioning and publishing can now be managed!
-      `.trim(),
-    );
-  }
+    `.trim(),
+  );
 }
