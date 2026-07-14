d70ea186aa8eb5c812c5bdc13cba498eb10a16ec
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-07-14 23:52:16 +0200

    allowOnly: true

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    allowOnly: true,
     experimental: { preParse: true },
     clearMocks: true,
     restoreMocks: true,
