902d91b57211ca88fba7181a5e26e12b58ec46fd
bq <adam@haglund.dev>
2026-07-20 09:44:59 +0200

    move migration file, start adding changeset references

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -179,7 +179,6 @@ function getMainSidebar(): DefaultTheme.SidebarItem[] {
         { text: "Getting Started", link: "getting-started" },
         { text: "Why Changesets", link: "why" },
         { text: "Technical Decisions", link: "technical-decisions" },
-        { text: "Migration from v2", link: "migration" },
       ],
     },
     {
@@ -212,6 +211,7 @@ function getMainSidebar(): DefaultTheme.SidebarItem[] {
         },
         { text: "Snapshot Releases", link: "snapshot-releases" },
         { text: "Prereleases", link: "prereleases" },
+        { text: "Migrating to v3", link: "migration/v3" },
         { text: "Beyond npm", link: "beyond-npm" },
       ],
     },
