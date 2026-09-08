6c500d6ec33d0d6049dacfd01c29a226bbfb5cb3
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2026-09-08 15:48:57 +0200

    test: adapt version command snapshot

--- packages/cli/src/commands/version/version.test.ts
+++ packages/cli/src/commands/version/version.test.ts
@@ -2456,6 +2456,8 @@ describe("updateInternalDependents: always", () => {
       ### Patch Changes
 
       - g1th4sh: This is some fix
+      - Updated dependencies [g1th4sh]
+        - pkg-a@1.1.0
       "
     `);
   });
